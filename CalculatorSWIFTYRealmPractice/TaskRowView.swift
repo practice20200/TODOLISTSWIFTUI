0c9c295edd33f138f95e7bd997e16dda0bb5775a
practice20200 <wor9hard2018@gmail.com>
2022-02-09 00:30:09 -0800

    3rd Commit Created TaskRowView

---
+++ CalculatorSWIFTYRealmPractice/TaskRowView.swift
@@ -0,0 +1,38 @@
+//
+//  TaskRowView.swift
+//  CalculatorSWIFTYRealmPractice
+//
+//  Created by Apple New on 2022-02-09.
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+    let task : Task
+    
+    var body: some View {
+        
+        HStack(spacing: 8){
+            Button{
+                print("Action handler")
+            }label: {
+                Image(systemName: task.completed ? "check.circle.fill" : "circle")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(task.completed ? .green: .gray)
+            }
+            
+            Text(task.title)
+                .foregroundColor(.black)
+            
+            Spacer()
+        }.padding(EdgeInsets(top: 16, leading: 20, bottom: 16, trailing: 20))
+        
+    }
+}
+
+struct TaskRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskRowView(task: Task(id: "1", title: "swimmimg", completed: false))
+    }
+}
