9e78c8b2026275290b726840d30e8b28519ffa26
practice20200 <wor9hard2018@gmail.com>
2022-02-08 23:24:07 -0800

    Initial Commit

---
+++ CalculatorSWIFTYRealmPractice/CalculatorSWIFTYRealmPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSWIFTYRealmPracticeApp.swift
+//  CalculatorSWIFTYRealmPractice
+//
+//  Created by Apple New on 2022-02-08.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSWIFTYRealmPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
