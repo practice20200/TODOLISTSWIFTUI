a9528cc123586693422d0b219082a7dd7b2dbc3e
practice20200 <wor9hard2018@gmail.com>
2022-02-10 19:51:06 -0800

    11th Commit Added Duedate

--- CalculatorSWIFTYRealmPractice/Task.swift
+++ CalculatorSWIFTYRealmPractice/Task.swift
@@ -11,11 +11,23 @@ struct Task : Identifiable {
     var title: String
     var completed: Bool
     var completedAt: Date = Date()
+    var dueDate: Date? = nil
     
     init(taskObject: TaskObject){
         self.id = taskObject.id.stringValue
         self.title = taskObject.title
         self.completed = taskObject.completed
         self.completedAt = taskObject.completedAt
+        self.dueDate = taskObject.dueDate
+    }
+    
+    var formattedDate: String{
+        if let date = dueDate{
+            let format = "MMM, d, y"
+            let dateformatter = DateFormatter()
+            dateformatter.dateFormat = format
+            return "Due at: " + dateformatter.string(from: date)
+        }
+        return ""
     }
 }
--- CalculatorSWIFTYRealmPractice/TaskObject.swift
+++ CalculatorSWIFTYRealmPractice/TaskObject.swift
@@ -13,4 +13,6 @@ class TaskObject: Object{
     @Persisted var title: String
     @Persisted var completed: Bool = false
     @Persisted var completedAt: Date = Date()
+    
+    @Persisted var dueDate: Date? = nil
 }
